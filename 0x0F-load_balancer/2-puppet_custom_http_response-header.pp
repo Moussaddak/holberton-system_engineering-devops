1c3aa348467b4d1d28061531e0bb377adf0dc74f
moussaddak <meddeb.moussaddak@gmail.com>
2020-08-14 22:30:48 +0000

    run puppet

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -0,0 +1,10 @@
+# Add a custom HTTP header with Puppet
+package {'nginx':
+ensure => present,
+}
+service {'nginx':
+ensure  => running,
+}
+exec {'hostname=$(echo $HOSTNAME); sudo sed -i "0,/http {/{s/http {/http {\n\tadd_header X-Served-By \"$hostname\"/}" /etc/nginx/nginx.conf; sudo service nginx restart':
+provider => 'shell',
+}
