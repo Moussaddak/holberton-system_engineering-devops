6e80d9c490137e347fadc109daf3142ba980aca0
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-08-15 00:50:25 +0200

    Update 2-puppet_custom_http_response-header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -5,6 +5,3 @@ ensure => present,
 service {'nginx':
 ensure  => running,
 }
-exec {'hostname=$(cat /etc/hostname) ; sudo sed -i "0,/http {/{s/http {/http {\n\tadd_header X-Served-By \"$hostname\"/}" /etc/nginx/nginx.conf; sudo service nginx restart':
-provider => 'shell',
-}
