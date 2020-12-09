0345e679f59a0ac4fce4af489ac64bc2b1274bcc
Moussaddak <52888741+Moussaddak@users.noreply.github.com>
2020-12-09 23:49:27 +0100

    Update 7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -11,7 +11,7 @@ service { 'nginx':
 
 file_line { '/etc/nginx/sites-available/default':
   ensure => 'present',
-  path   => '/etc/nginx/sites-available/default'
+  path   => '/etc/nginx/sites-available/default',
   after  => 'listen 80 default_server;',
   line   => 'location /redirect_me {\n\t\terror_page 404 =301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\t}',
 }
