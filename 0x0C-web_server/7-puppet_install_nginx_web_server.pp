00b610fa09b8fbca95d62cca3d7c2d374b3940ac
moussaddak <meddeb.moussaddak@gmail.com>
2020-12-09 22:28:15 +0000

    puppet manefist

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,20 @@
+# Install Nginx web server (w/ Puppet)
+
+package { 'nginx':
+  ensure => installed,
+}
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+
+file_line { '/etc/nginx/sites-available/default':
+  ensure => 'present',
+  after  => 'listen [::]:80 default_server;',
+  line   => 'error_page 404 =301 https://www.youtube.com/watch?v=QH2-TGUlwu4',
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+  content => 'Holberton School',
+}
