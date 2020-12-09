3e685d62e9b21a22c766bf419d55875baea66b86
moussaddak <meddeb.moussaddak@gmail.com>
2020-12-09 11:40:48 +0000

    Revert "refactor script"
    This reverts commit a17474cc024a6c65d4dbe81fd62a9f7885499f2d.

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,11 +1,12 @@
-# Set up client SSH configuration file
-
-file_line { 'puppet_ssh_config':
-  ensure => 'present',
-  path   => '/etc/ssh/ssh_config',
-  replace => true,
-  line   => 'PasswordAuthentication no',
-  match  => '^#   PasswordAuthentication yes$',
+# SSH client configuration via Puppet 
+file_line { 'NO passwd':
+ensure => 'present',
+path   => '/etc/ssh/ssh_config',
+line   => 'PasswordAuthentication no',
 }
 
-
+file_line { 'add public key':
+ensure => 'present',
+path   => '/etc/ssh/ssh_config',
+line   => 'IdentityFile ~/.ssh/holberton',
+}
