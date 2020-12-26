d05769d0ac8c64a297609cd9c996abc9ef31b4c0
moussaddak <meddeb.moussaddak@gmail.com>
2020-12-26 13:07:56 +0000

    CM puppet file added

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,7 +1,50 @@
 # Add a custom HTTP header with Puppet
-package {'nginx':
-ensure => present,
+$str="server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
+	
+	root /var/www/html;
+        index index.html index.htm index.nginx-debian.html;
+	
+	error_page 404 /404.html;
+        
+	add_header X-Served-By $HOSTNAME always;
+	
+	location /redirect_me {
+                error_page 404 =301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        }
+}"
+
+exec { 'update':
+  command  => '/usr/bin/sudo apt-get update',
 }
-service {'nginx':
-ensure  => running,
+
+package { 'nginx':
+   require => Exec['update'],
+   ensure  => installed,
+}
+
+service { 'nginx':
+    ensure => stopped,
+}
+
+file { '/etc/nginx/sites-available/default':
+    require => Package['nginx'],
+    content => $str,
+}
+
+file { '/var/www/html/index.nginx-debian.html':
+    ensure  => present,
+    content => 'Holberton School'
+}
+
+file { '/var/www/html/404.html':
+    ensure  => present,
+    content => "Ceci n'est pas une page",
+}
+
+exec { 'activate nginx':
+    require  => File['/etc/nginx/sites-available/default'],
+    command  => 'service nginx start',
+    provider => shell,
 }
