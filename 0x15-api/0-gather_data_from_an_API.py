2b59c87a8db31e206bcd21cac387acf4429b696e
moussaddak <meddeb.moussaddak@gmail.com>
2021-02-01 18:52:04 +0000

    first commit

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" Gather data from an API  """
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com'
+    employee_id = argv[1]
+    """ Extract Employee name """
+    user = requests.get('{}/users/{}'.format(url, employee_id))
+    employee_name = user.json().get('name')
+    """ Extract Employee Tasks """
+    todos = requests.get('{}/todos?userId={}'.format(url, employee_id))
+    result = todos.json()
+    """ Extract List of completed Tasks of Employee """
+    Done = [Task for Task in result if Task.get('completed')]
+    """ Result """
+    print('Employee {} is done with tasks({}/{}):'.
+          format(employee_name, len(Done), len(result)))
+    for Task in Done:
+        print('\t{}'.format(Task.get('title')))
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""  Export to CSV  """
+import csv
+import requests
+from sys import argv
+
+if __name__ == "__main__":
+    url = 'https://jsonplaceholder.typicode.com'
+    USER_ID = 2  # argv[1]
+    """ Extract Employee name """
+    user = requests.get('{}/users/{}'.format(url, USER_ID)).json()
+    USERNAME = user.get('username')
+    """ Extract Employee Tasks """
+    todos = requests.get('{}/todos?userId={}'.format(url, USER_ID)).json()
+    rows = []
+    for Task in todos:
+        TASK_COMPLETED_STATUS = Task.get("completed")
+        TASK_TITLE = Task.get("title")
+        rows.append([USER_ID, USERNAME, TASK_COMPLETED_STATUS, TASK_TITLE])
+    filename = "%s.csv" % USER_ID
+    """ export to csv file """
+    with open(filename, 'wt') as f:
+        csv_writer = csv.writer(f, quoting=csv.QUOTE_ALL)
+        [csv_writer.writerow(row) for row in rows]
