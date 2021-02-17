550bcd1e7e2125fad65595bd9940e995be4ed0b6
moussaddak <meddeb.moussaddak@gmail.com>
2021-02-17 12:00:51 +0000

    debug

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # 0. Strace is your friend
 exec {'debugging':
-  command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
-  path   => '/bin'
+    command => "sed -i 's/phpp/php/g' /var/www/html/wp-settings.php",
+    path    => '/bin'
 }
