902c86bb129e02174d1fb89e61810353175cea78
moussaddak <meddeb.moussaddak@gmail.com>
2021-02-25 21:03:03 +0000

    --fix

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,6 +1,6 @@
 # 1. User limit
 
 exec { 'debugging':
-    command  => 'sudo echo "fs.file-max = 65536" >> /etc/sysctl.conf ; sudo sysctl -p',
-    path => '/usr/bin',
+    command => 'sudo echo "fs.file-max = 65536" >> /etc/sysctl.conf ; sudo sysctl -p',
+    path    => '/usr/bin',
 }
