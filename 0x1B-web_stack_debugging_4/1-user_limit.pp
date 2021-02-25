816b91d7710138c15940da0263ad969dfa8d5e29
Moussaddak Meddeb <52888741+Moussaddak@users.noreply.github.com>
2021-02-25 22:17:39 +0100

    Update 1-user_limit.pp

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,5 +2,5 @@
 
 exec { 'debugging':
     command => 'sudo echo "fs.file-max = 65536" >> /etc/sysctl.conf ; sudo sysctl -p',
-    provider => 'shell',
+    path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
